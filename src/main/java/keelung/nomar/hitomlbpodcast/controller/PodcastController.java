39c87f3fc9be5503a8e9100eb2b4aac699d85cec
Albert Ma <shangpinma@gmail.com>
2026-03-01 13:51:25 +0800

    fix: update the recommendation list

--- src/main/java/keelung/nomar/hitomlbpodcast/controller/PodcastController.java
+++ src/main/java/keelung/nomar/hitomlbpodcast/controller/PodcastController.java
@@ -27,10 +27,11 @@ public class PodcastController {
     @GetMapping("/recommended-keywords")
     public List<String> getRecommendedKeywords() {
         return Arrays.asList(
-                "紅襪", "洋基", "道奇", "大都會", "教士", "太空人", "雙城", "光芒", "水手", "勇士",  "天使", "費城人", "運動家", "白襪", 
+                "紅襪", "洋基", "道奇", "大都會", "教士", "太空人", "雙城", "光芒", "水手", "勇士",  "天使", "費城人", "運動家", "白襪",
                 "大谷翔平", "Judge", "Soto", "Raleigh", "Devers", "Betts", "Harper",
                 "Crochet", "Skubal", "Skenes", "山本由伸", "Kershaw", "Verlander", "Scherzer",
-                "張育成", "鄭宗哲"
+                "張育成", "鄭宗哲",
+                "好書我來讀","好劇我來看","人物我來講","轉學生週記"
         );
     }
 }
